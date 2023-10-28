2602dabd1f8b8ca77cdb385d0589e48239797d8b
Sharad Kevadiya <64027328+sharad-999@users.noreply.github.com>
2023-10-28 14:08:40 +0530

    setup typeORM

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,17 @@
 import { config } from 'dotenv';
-config();
+import path from 'path';
+config({ path: path.join(__dirname, `../../.env/${process.env.NODE_ENV}`) });
 
 // eslint-disable-next-line no-undef
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+    process.env;
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
 };
